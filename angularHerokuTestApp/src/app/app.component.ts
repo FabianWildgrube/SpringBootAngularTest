550fae95ccbdcb04814306c292377eba64fe79ac
FabianWildgrube <fabian.wildgrube@gmx.de>
2019-11-20 19:30:38 +0100

    added basic echarts example; using nebular theme

--- angularHerokuTestApp/src/app/app.component.ts
+++ angularHerokuTestApp/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { EChartOption } from 'echarts';
+import { _XAxis } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,74 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularHerokuTestApp';
+  data = [[0, 1400], [1, 342], [2, 1120]];
+  nextDataIndex = 3;
+
+  randomValuesSeriesName = "randomValues";
+
+  clickedValue = {
+    x: null,
+    y: null
+  };
+
+  chartOption : EChartOption = {
+    title: {
+      text: "A fancy title",
+      subtext: "And a subtitle",
+      itemGap: 5,
+      left: 'center'
+    },
+    legend: {
+      data: [{
+        name: this.randomValuesSeriesName,
+        icon: 'point',
+        // set up the text in red
+        textStyle: {
+            color: '#304523'
+        }
+      }],
+      bottom: 10
+    },
+    xAxis: {
+      type: 'category'
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        name: this.randomValuesSeriesName,
+        type: 'line',
+        smooth: true,
+        itemStyle: { normal: { areaStyle: { type: 'default' } } },
+        data: this.data
+      }
+    ]
+  }
+
+  mergeOption : any;
+
+  onChartClick(event: any): void {
+    console.log(event);
+    this.clickedValue.x = event.data[0];
+    this.clickedValue.y = event.data[1];
+  }
+
+  addDataPoint() : void {
+    let newData = this.data;
+
+    newData.push([this.nextDataIndex++, Math.random() * 1500]);
+
+    this.mergeOption = {
+      series: [
+        {
+          name: this.randomValuesSeriesName,
+          type: 'line',
+          smooth: true,
+          itemStyle: { normal: { areaStyle: { type: 'default' } } },
+          data: newData
+        }
+      ]
+    }
+  }
 }
--- angularHerokuTestApp/src/app/app.module.ts
+++ angularHerokuTestApp/src/app/app.module.ts
@@ -3,12 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { NgxEchartsModule } from 'ngx-echarts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbButtonModule, NbCardModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxEchartsModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbButtonModule,
+    NbCardModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
